d8d13a1cc15beac3aac0efaba171941fe14681e8
Yurii Kadirov <admin@sirkadirov.com>
2025-11-12 01:17:06 +0200

    Add 404 page not found route
    Signed-off-by: Yurii Kadirov <admin@sirkadirov.com>

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<main>
+    <div class="text-center py-15 px-4 sm:px-6 lg:px-8">
+        <h1 class="block text-7xl font-bold sm:text-9xl">404</h1>
+        <p class="mt-3 text-gray-600 dark:text-neutral-400">Цієї сторінки не знайдено</p>
+        <p class="text-gray-600 dark:text-neutral-400">Можливо, її вкрали іншопланетяни?</p>
+        <div class="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
+            <a href="/" class="gap-x-1 py-2 px-2.5 inline-flex items-center font-medium text-sm rounded-lg bg-blue-900 text-white hover:bg-blue-950 focus:outline-hidden focus:bg-blue-900">
+                <span class="material-symbols-outlined" style="font-size: 14pt;">home</span>
+                <span>На головну сторінку</span>
+            </a>
+        </div>
+    </div>
+</main>
